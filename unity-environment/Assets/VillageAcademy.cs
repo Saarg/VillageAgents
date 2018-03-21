e73ca20b914f9f1f42c79769570fed7860903897
Saarg <milsonneau.jean@free.fr>
2018-03-20 21:18:26 -0400

    adjusting values

--- unity-environment/Assets/VillageAcademy.cs
+++ unity-environment/Assets/VillageAcademy.cs
@@ -114,12 +114,12 @@ public class VillageAcademy : Academy {
         }
 
         GameObject farm = farms[Random.Range(0, farms.Count)].gameObject;
-        if (food < 400f && !jobOffers.Contains(farm)) {
+        if (Random.Range(food - 250f, food) < 250f && !jobOffers.Contains(farm)) {
             jobOffers.Enqueue(farm);
         }
 
         GameObject c = coalPlants[Random.Range(0, coalPlants.Count)].gameObject;
-        if (coal < 400f && !jobOffers.Contains(c)) {
+        if (Random.Range(coal - 250f, coal) < 250f && !jobOffers.Contains(c)) {
             jobOffers.Enqueue(c);
         }
     }
